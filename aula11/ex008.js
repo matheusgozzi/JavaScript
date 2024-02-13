261ea3874d3826249cce49d4d9ab49e75677d6ed
matheusgozzi <matheus.0205@hotmail.com>
2024-02-13 15:19:44 -0300

    Realizado exercícios de condicionais em Js

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 78
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'brasil'
+console.log(`Você está vivendo em ${pais}`)
+
+if( pais =='Brasil'){
+console.log('Você é Brasileiro!')
+}else{
+console.log('Você é estrangeiro!')
+
+}
