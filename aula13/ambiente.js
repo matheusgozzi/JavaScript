d314bf6793b836866d6ac46a263ab54eb4b3ae45
matheusgozzi <matheus.0205@hotmail.com>
2024-02-14 15:18:06 -0300

    Feito aulas de estruturas de repeticao

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+// var contador = 1
+// while(contador <= 6){
+//     console.log(`repetindo ${contador} vez.`)
+//     contador++
+// }
+
+var contador = 1
+do{
+    console.log(`Fazendo a repetição ${contador} vezes`)
+    contador++
+} while (contador <= 5)
